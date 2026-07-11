2ed48fd4b691d6b66ebb3bf25481ee8114353649
zph <zphhpzzph@gmail.com>
2026-07-11 18:50:31 +0800

    fix: use hash history for GitHub Pages SPA routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Portal from '../views/Portal.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
@@ -74,7 +74,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
